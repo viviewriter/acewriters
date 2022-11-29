9e5d751348d6750b7bece893d6d5a030eba6debf
mong'are <mongaresams@gmail.com>
2022-11-29 07:16:42 +0300

    Add folder structure and components shell

---
+++ src/components/callToActionBtns/Cta.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Cta() {
+  return (
+    <div className="cta">
+      <h1>Hi from about</h1>
+    </div>
+  );
+}
+
+export default Cta;
---
+++ src/components/experience/Experience.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Experience = () => (
+  <div>Experience</div>
+);
+
+export default Experience;
