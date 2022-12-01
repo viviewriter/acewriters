3e2176f222fbed26abce7f3805cb8d530b6ad360
mong'are <mongaresams@gmail.com>
2022-12-01 13:13:36 +0300

    Add images to the assets folder and more dependencies in package

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
 import About from './components/about/About';
-import Experience from './components/experience/Experience';
 import Services from './components/services/Services';
 import Portfolio from './components/portfolio/Portfolio';
 import Testimonials from './components/testimonials/Testimonials';
@@ -14,7 +13,6 @@ const App = () => (
     <Nav />
     <Header />
     <About />
-    <Experience />
     <Services />
     <Portfolio />
     <Testimonials />
