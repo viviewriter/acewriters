3d32115a6da5ea55836c7e75e145f9092d7ef14d
mong'are <mongaresams@gmail.com>
2022-12-01 13:06:05 +0300

    Update the testimonials components and itsstyling

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
+import { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
 import './testimonials.css';
+import dataTestimonials from '../../data/dataTestimonials';
 
-function Testimonials() {
-  return (
-    <div>Testimonials</div>
-  );
-}
+const Testimonials = () => (
+  <section id="testimonials">
+    <h5>Review from clients</h5>
+    <h2>Testimonials</h2>
+    <Swiper
+      className="container testimonials__container"
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+    >
+      {
+        dataTestimonials.map(({
+          id, userName, avatar, review,
+        }) => (
+          <SwiperSlide
+            className="testimonial owl-carousel"
+            key={id}
+          >
+            <div className="pic">
+              <img src={avatar} alt="Avatar one" />
+            </div>
+            <p className="description">
+              {review}
+            </p>
+            <div className="testimonial-profile">
+              <h3 className="title">{userName}</h3>
+              <br />
+              <span className="post">Web Developer</span>
+            </div>
+          </SwiperSlide>
+        ))
+      }
+    </Swiper>
+    <hr />
+  </section>
+);
 
 export default Testimonials;
