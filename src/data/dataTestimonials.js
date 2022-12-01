46c95043c788f3e69392cd05cc781f7b3e59b501
mong'are <mongaresams@gmail.com>
2022-12-01 13:15:38 +0300

    Add data folder for testimonials and portfolio data

---
+++ src/data/dataTestimonials.js
@@ -0,0 +1,43 @@
+import AVTR1 from '../assets/avatar1.jpg';
+import AVTR2 from '../assets/avatar2.jpg';
+import AVTR3 from '../assets/avatar3.jpg';
+import AVTR4 from '../assets/avatar4.jpg';
+
+export default [
+  {
+    id: 1,
+    avatar: AVTR1,
+    userName: 'Jane Doe',
+    review: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consectetur modi explicabo provident atque odio excepturi
+            perspiciatis dignissimos a incidunt nobis itaque culpa aut
+            porro, magnam, nisi labore, suscipit sunt repellendus!`,
+  },
+  {
+    id: 2,
+    avatar: AVTR2,
+    userName: 'John Doe',
+    review: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consectetur modi explicabo provident atque odio excepturi
+            perspiciatis dignissimos a incidunt nobis itaque culpa aut
+            porro, magnam, nisi labore, suscipit sunt repellendus!`,
+  },
+  {
+    id: 3,
+    avatar: AVTR3,
+    userName: 'Jacob Doe',
+    review: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consectetur modi explicabo provident atque odio excepturi
+            perspiciatis dignissimos a incidunt nobis itaque culpa aut
+            porro, magnam, nisi labore, suscipit sunt repellendus!`,
+  },
+  {
+    id: 4,
+    avatar: AVTR4,
+    userName: 'Jullie Doe',
+    review: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consectetur modi explicabo provident atque odio excepturi
+            perspiciatis dignissimos a incidunt nobis itaque culpa aut
+            porro, magnam, nisi labore, suscipit sunt repellendus!`,
+  },
+];
