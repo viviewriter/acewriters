9f1b256a8971eaf295521f2d2b57c1a66a2253bf
mong'are <mongaresams@gmail.com>
2022-12-03 01:17:06 +0300

    Update footer links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => (
 
     <ul className="permalinks">
       <li><a href="#">Home</a></li>
-      <li><a href="about">About</a></li>
+      <li><a href="#about">About</a></li>
       <li><a href="#services">Services</a></li>
       <li><a href="#portfolio">Portfolio</a></li>
       <li><a href="#testimonials">Testimonials</a></li>
