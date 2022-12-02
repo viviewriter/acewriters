992940c2447b4e26d04832b0399012ccfa4a95bb
mong'are <mongaresams@gmail.com>
2022-12-02 18:22:40 +0300

    Update the contact section of the app

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_2ua7gsv', 'template_p5f5ehc', form.current, 'pZ9pl1IiGmIcTva4d')
+    emailjs.sendForm('service_fz35ciy', 'template_cje7kuy', form.current, 'DkFlynbzQ5AVWQWNT')
       .then((result) => result.text, (error) => error.text);
 
     e.target.reset();
@@ -26,24 +26,24 @@ const Contact = () => {
           <article className="contact__option">
             <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
-            <h5>dummy@mail.com</h5>
-            <a href="mailto:dummy@mail.com" target="_blank" rel="noreferrer">send a message</a>
+            <h5>chegsolutions3@gmail.com</h5>
+            <a href="mailto:chegsolutions3@gmail.com" target="_blank" rel="noreferrer">send a message</a>
           </article>
           <article className="contact__option">
             <RiMessengerLine className="contact__option-icon" />
-            <h4>Messenger</h4>
-            <h5>mongare</h5>
-            <a href="facebook/profile" target="_blank" rel="noreferrer">send a message</a>
+            <h4>Instagram</h4>
+            <h5>Ace Writers</h5>
+            <a href="https://www.instagram.com/ace_writers_" target="_blank" rel="noreferrer">send a message</a>
           </article>
           <article className="contact__option">
             <BsWhatsapp className="contact__option-icon" />
             <h4>WhatsApp</h4>
-            <h5>+254762992818</h5>
-            <a href="https://api.whatsapp.com/send?phone=+254704381982" target="_blank" rel="noreferrer">send a message</a>
+            <h5>+1 269 286 6924</h5>
+            <a href="https://api.whatsapp.com/send?phone+12692866924" target="_blank" rel="noreferrer">send a message</a>
           </article>
         </div>
         <form ref={form} onSubmit={sendEmail}>
-          <input hidden readOnly type="text" value="Sam" name="name" />
+          <input hidden readOnly type="text" value="Ace Writers" name="name" />
           <input type="text" name="from_name" placeholder="Your Full Name" required />
           <input type="email" name="email" placeholder="Enter your email" required />
           <textarea name="message" rows="7" placeholder="Your Message" required />
