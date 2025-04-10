8dc279185356ab911ef41bf02cc3752ec5d91191
viviewriter <viviethewriter@gmail.com>
2025-04-10 18:12:48 +0300

    more changes

--- src/App.jsx
+++ src/App.jsx
@@ -14,16 +14,15 @@ const App = () => {
 
    useEffect(() => {
      axios
-	    .get('http://127.0.0.1:8000/api/hello/')
-	   .then((response) => {
-		  setMessage(response.data.message);
-		})
-		.catch((error) => console.error('Error fetching data', error));
-	}, []);
-
-	return (
+     .get('http://127.0.0.1:8000/api/hello/')
+     .then((response) => {
+      setMessage(response.data.message);
+    })
+    .catch((error) => console.error('Error fetching data', error));
+  }, []);
+  return (
   <div>
-    <h1>React and Django</h1>
+    <h1>Welcome to Ace</h1>
     <p>{message}</p>
     <Nav />
     <Header />
@@ -34,7 +33,7 @@ const App = () => {
     <Contact />
     <Footer />
   </div>
-	);
+);
   };
 
   export default App;
